a04bdff8bca38460f4f823e9e91548f77c454593
Christof Stocker <stocker.christof@gmail.com>
2017-12-21 14:43:30 +0100

    add some basic functions

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ConvUtils
 
-[![Build Status](https://travis-ci.org/Evizero/ConvUtils.jl.svg?branch=master)](https://travis-ci.org/Evizero/ConvUtils.jl)
+[![Build Status](https://travis-ci.org/Evizero/ConvUtils.jl.svg?branch=master)](https://travis-ci.org/Evizero/ConvUtils.jl) [![codecov.io](http://codecov.io/github/Evizero/ConvUtils.jl/coverage.svg?branch=master)](http://codecov.io/github/Evizero/ConvUtils.jl?branch=master)
 
-[![Coverage Status](https://coveralls.io/repos/Evizero/ConvUtils.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/Evizero/ConvUtils.jl?branch=master)
-
-[![codecov.io](http://codecov.io/github/Evizero/ConvUtils.jl/coverage.svg?branch=master)](http://codecov.io/github/Evizero/ConvUtils.jl?branch=master)
+Small utility package for working and visualizing convolutional
+neural networks. The package is not really intended to be used by
+anyone but me, but feel free to do so according to the license.
+This is just a different way of saying that I made no effort in
+making this package clean, well documented, generic, or reusable.
--- src/ConvUtils.jl
+++ src/ConvUtils.jl
@@ -1,5 +1,48 @@
 module ConvUtils
 
-# package code goes here
+using Images
+using MosaicViews
+using MappedArrays
+using MLDataUtils
+
+const DEFAULT_COLORNEG = RGB(0.230, 0.299, 0.754)
+const DEFAULT_COLORZRO = RGB(0.95,  0.95,  0.95)
+const DEFAULT_COLORPOS = RGB(0.706, 0.016, 0.150)
+
+export
+
+    falsecolor,
+    upscale,
+    immosaic
+
+function falsecolor(A::Array{T,N};
+                    symmetric = true,
+                    colorneg  = DEFAULT_COLORNEG,
+                    colorzero = DEFAULT_COLORZRO,
+                    colorpos  = DEFAULT_COLORPOS) where {T<:Number,N}
+    mi, ma = extrema(A)
+    val = max(abs(mi), ma)
+    ss = if symmetric
+        scalesigned(-val, T(0), val)
+    else
+        scalesigned(mi, T(0), ma)
+    end
+    x = ss(one(T))
+    cs = colorsigned(map(c->convert(RGB{typeof(x)},c), (colorneg, colorzero, colorpos))...)
+    mappedarray(c->cs(ss(c)), A)
+end
+
+falsecolor(A; kw...) = falsecolor(Array(A); kw...)
+
+function upscale(A::AbstractArray{T,N}, val::Int) where {T,N}
+    repeat(A, inner=(val, val, ntuple(i->1, N-2)...))
+end
+
+function immosaic(A::AbstractArray, fill=RGB(1.,1.,1.); rep=1, npad=5, ncol=-1, nrow=-1)
+    if ndims(A) == 3 && ncol == -1 && nrow == -1
+        ncol = max(ceil(Int,sqrt(size(A,3))), 5)
+    end
+    mosaicview(upscale(A, rep), fill; npad=npad, nrow=nrow, ncol=ncol, rowmajor=true)
+end
 
 end # module
